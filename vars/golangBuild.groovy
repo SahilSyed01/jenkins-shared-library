604c6f8ddad1c198c2c4007b4b40633d7ad9f874
SahilSyed01 <sahilsyed7781@gmail.com>
2024-07-25 15:43:57 +0530

    changes

--- vars/golangBuild.groovy
+++ vars/golangBuild.groovy
@@ -1,24 +1,21 @@
-#!groovy
-
-def call(){
-  pipeline{
-          agent any
-          stages{
-           stage('Build'){
-            steps{
-             script{
-               go build .
-          }
-        }
-
-      }
-      stage('test'){
-          steps{
-            script{
-              go test ./... -cover
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('Build') {
+                steps {
+                    script {
+                        go build .
+                    }
+                }
             }
-          }
+            stage('Test') {
+                steps {
+                    script {
+                        go test ./... -cover
+                    }
+                }
+            }
         }
     }
-  }
 }
